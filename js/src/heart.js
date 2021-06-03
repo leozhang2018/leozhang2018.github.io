c02962bfb81e13dda01f09c84eed44ffcd309fe0
leozhang2018 <leozhang2018@gmail.com>
2021-06-03 21:21:05 +0800

    Site updated: 2021-06-03 21:21:04

--- js/src/heart.js
+++ js/src/heart.js
@@ -1,3 +1,3 @@
-// build time:Fri Mar 22 2019 21:00:57 GMT+0800 (CST)
+// build time:Thu Jun 03 2021 21:20:52 GMT+0800 (GMT+08:00)
 $(".heart").on("click touchstart",function(){$(this).toggleClass("is_animating")}),$(".heart").on("animationend",function(){$(this).toggleClass("is_animating")});
 //rebuild by neat 
--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Fri Mar 22 2019 21:00:57 GMT+0800 (CST)
+// build time:Thu Jun 03 2021 21:20:52 GMT+0800 (GMT+08:00)
 $(document).ready(function(){var o,i=window.location.href.replace(window.location.origin,"");if($(window).on("scroll",function(){clearTimeout(o),o=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+i,{expires:365,path:""})},250)}),void 0!==Cookies.get("scroll-cookie")){var e=Cookies.get("scroll-cookie").split("|");e[1]==i&&$(window).scrollTop(e[0])}});
 //rebuild by neat 
