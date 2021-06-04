a6239bb31843a46421a22568fe324485a2f848a4
leozhang2018 <leozhang2018@gmail.com>
2021-06-04 11:55:01 +0800

    Site updated: 2021-06-04 11:55:00

--- js/src/heart.js
+++ js/src/heart.js
@@ -1,3 +1,3 @@
-// build time:Fri Jun 04 2021 11:27:25 GMT+0800 (GMT+08:00)
+// build time:Fri Jun 04 2021 11:54:46 GMT+0800 (GMT+08:00)
 $(".heart").on("click touchstart",function(){$(this).toggleClass("is_animating")}),$(".heart").on("animationend",function(){$(this).toggleClass("is_animating")});
 //rebuild by neat 
